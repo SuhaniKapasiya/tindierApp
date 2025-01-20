c9d604125ca2c7367b411698273036d3931c0511
SuhaniKapasiya <107610361+SuhaniKapasiya@users.noreply.github.com>
2025-01-21 00:03:39 +0530

    exploring get,post ,delete

--- src/app.js
+++ src/app.js
@@ -2,17 +2,43 @@ const express = require("express");
 
 const app = express();
 
+
+
+
+
+
+app.get("/user",(req,res)=>{
+
+    res.send({firstName:"suhani",lastName :"Kapasiya"});
+
+})
+
+
+
+app.post("/user", (req, res) => {
+    console.log("Save Data to the database");
+    
+  res.send("Data successfully saved to the database");
+});
+
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully");
+});
+
+
+app.use("/user", (req, res) => {
+  res.send("hahahaha");
+});
+
+
+//this will match all the HTTP method API calls to /test
 app.use("/text", (req, res) => {
   res.send("Hello Hello Hello---------------- ");
 });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello suhani kapasiya---------------------");
-});
 
-app.use("/", (req, res) => {
-  res.send("Hello form the server"); //req handler
-});
+
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");
