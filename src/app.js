5a917f1a5640744973776c3eb3d20eec6ef7524e
SuhaniKapasiya <107610361+SuhaniKapasiya@users.noreply.github.com>
2025-01-24 20:18:25 +0530

    dynamic route

--- src/app.js
+++ src/app.js
@@ -2,43 +2,49 @@ const express = require("express");
 
 const app = express();
 
+// app.get(/.*fly$/, (req, res) => {
+//   res.send("Hello Suhani");
+// });
+
+// app.get(/a/, (req, res) => {
+//   res.send("Hello Suhani");
+// });
+
+//// This will only handle GET call to user
+// app.get("/user", (req, res) => {
+//   console.log("user id", req.query);
+
+//   res.send({ firstName: "suhani", lastName: "Kapasiya" });
+// });
 
 
+// How to handle Dynamic route 
 
+app.get("/user/:userId",(req,res)=>{
+  console.log(req.params);
 
-
-app.get("/user",(req,res)=>{
-
-    res.send({firstName:"suhani",lastName :"Kapasiya"});
-
+    res.send({ firstName: "suhani", lastName: "Kapasiya" });
 })
 
 
+// app.post("/user", (req, res) => {
+//     console.log("Save Data to the database");
 
-app.post("/user", (req, res) => {
-    console.log("Save Data to the database");
-    
-  res.send("Data successfully saved to the database");
-});
+//   res.send("Data successfully saved to the database");
+// });
 
+// app.delete("/user", (req, res) => {
+//   res.send("Deleted successfully");
+// });
 
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully");
-});
+// app.use("/user", (req, res) => {
+//   res.send("hahahaha");
+// });
 
-
-app.use("/user", (req, res) => {
-  res.send("hahahaha");
-});
-
-
-//this will match all the HTTP method API calls to /test
-app.use("/text", (req, res) => {
-  res.send("Hello Hello Hello---------------- ");
-});
-
-
-
+// //this will match all the HTTP method API calls to /test
+// app.use("/text", (req, res) => {
+//   res.send("Hello Hello Hello---------------- ");
+// });
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");
