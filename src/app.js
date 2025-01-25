86a7ff539edccc5dfb51bf5036218717c68835ea
SuhaniKapasiya <107610361+SuhaniKapasiya@users.noreply.github.com>
2025-01-25 10:37:04 +0530

    multiple-route-handler

--- src/app.js
+++ src/app.js
@@ -2,49 +2,42 @@ const express = require("express");
 
 const app = express();
 
-// app.get(/.*fly$/, (req, res) => {
-//   res.send("Hello Suhani");
-// });
-
-// app.get(/a/, (req, res) => {
-//   res.send("Hello Suhani");
-// });
-
-//// This will only handle GET call to user
-// app.get("/user", (req, res) => {
-//   console.log("user id", req.query);
-
-//   res.send({ firstName: "suhani", lastName: "Kapasiya" });
-// });
-
-
-// How to handle Dynamic route 
-
-app.get("/user/:userId",(req,res)=>{
-  console.log(req.params);
-
-    res.send({ firstName: "suhani", lastName: "Kapasiya" });
-})
-
-
-// app.post("/user", (req, res) => {
-//     console.log("Save Data to the database");
-
-//   res.send("Data successfully saved to the database");
-// });
-
-// app.delete("/user", (req, res) => {
-//   res.send("Deleted successfully");
-// });
-
-// app.use("/user", (req, res) => {
-//   res.send("hahahaha");
-// });
-
-// //this will match all the HTTP method API calls to /test
-// app.use("/text", (req, res) => {
-//   res.send("Hello Hello Hello---------------- ");
-// });
+app.use("/user",
+ [ (req,res,next)=>{
+     console.log("Handling the route user 1!!"); // route handler
+  
+    //  res.send("Response1!")
+     next();
+    
+  }],
+   (req,res,next)=>{
+    console.log("Handling the route user 2!!"); // route ha  ndler
+    
+    // res.send("Response2!");
+      next();
+  
+   },
+   (req,res,next)=>{
+    console.log("Handling the route user 3!!"); // route ha  ndler
+    
+    // res.send("Response3!");
+      next();
+  
+   },
+  [ (req,res,next)=>{
+    console.log("Handling the route user 4!!"); // route ha  ndler
+      next();
+    // res.send("Response4!");
+  
+   },
+   (req,res,next)=>{
+    console.log("Handling the route user 5!!"); // route ha  ndler
+   ;
+    // res.send("Response5!");
+       next();
+  
+   }]
+)
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");
