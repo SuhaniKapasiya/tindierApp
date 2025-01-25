77ca56929c73719c23d988e0bb9413cb7d9499a3
SuhaniKapasiya <107610361+SuhaniKapasiya@users.noreply.github.com>
2025-01-25 17:11:01 +0530

    wild card error handling

--- src/app.js
+++ src/app.js
@@ -2,26 +2,53 @@ const express = require("express");
 
 const app = express();
 
-const {userAuth,adminAuth} = require("./middlewares/auht")
+// const {userAuth,adminAuth} = require("./middlewares/auht")
 
 
 
-app.use("/admin",adminAuth);
+// app.use("/admin",adminAuth);
+// // app.use("/user",userAuth);
 
-app.get("/user/login", (req, res) => {
-  res.send("User Loing Succesfully");
+// app.get("/user", userAuth, (req, res) => {
+//   res.send("User Data Sent");
+// });
+
+// app.get("/user/login", (req, res) => {
+//   res.send("User Loing Succesfully");
+// });
+
+// ;
+
+// app.get("/admin/getAllData",(req,res)=>{
+//   res.send("ALl Data sent")
+// })
+
+
+//ERROR  handling
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong");
+  }
 });
+app.get("/getUserData",(req,res)=>{
+    try{
+     
+      
+   throw new Error("dvbzhjf");
+   res.send("User Data Sent");
+    }catch(err){
+       res.status(500).send("Some Error contact support team")
+    }
 
-app.get("/user/data", userAuth, (req, res) => {
-  res.send("User Data Sent");
-});
-
-app.get("/admin/getAllData",(req,res)=>{
-  res.send("ALl Data sent")
 })
+// wild card error handling
 
-
-
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong");
+  }
+});
 
 
 app.listen(3000, () => {
